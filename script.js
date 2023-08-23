32157f4648f1a8d7647198e9c98ed15a842ca0d7
Miguel Gomez <miguelgomezca2021@gmail.com>
2023-08-23 09:11:10 -0400

    1 medium done

--- script.js
+++ script.js
@@ -2416,3 +2416,33 @@ console.log(canConstruct('a', 'b'))
 console.log(canConstruct('aa', 'ab'))
 
 console.log("----------------------------------------------")
+
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+//problem link: https://leetcode.com/problems/container-with-most-water/
+
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+    for(let i = 0; i < height.length; i++){
+      const area = Math.min(height[left], height[right]) * (right - left);
+      maxArea = Math.max(maxArea, area);
+      if(height[left] < height[right]){
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return maxArea;
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
