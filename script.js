5947ccf8e0da8828c382e0cd91caad3096793765
Miguel Gomez <miguelgomezca2021@gmail.com>
2023-08-29 12:20:01 -0400

    1 array easy done, felt like medium

--- script.js
+++ script.js
@@ -2486,3 +2486,28 @@ var getConcatenation = function(nums) {
   return [...arr1, ...arr2]
 };  
 console.log(getConcatenation([1,2,1]))
+
+console.log("----------------------------------------------")
+
+//problem link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+
+var removeDuplicates = function(nums) {
+  nums.sort((a, b) => a - b);
+
+  if (nums.length === 0) {
+      return 0;
+  }
+
+  let k = 1; 
+
+  for (let i = 1; i < nums.length; i++) {
+      if (nums[i] !== nums[i - 1]) {
+          nums[k] = nums[i]; 
+          k++;
+      }
+  }
+
+  return k;
+};
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+console.log(removeDuplicates([1,1,2]))
