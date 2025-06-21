f14ae7caaf8034b3b3f67666063bedd820209739
Miguel Gomez <miguelgomezca2021@gmail.com>
2025-06-20 23:56:58 -0400

    Pushing one LC med

--- script.js
+++ script.js
@@ -2947,4 +2947,25 @@ console.log("------------------------")
 
   console.log(twoSum(nums, target))
 
+console.log("------------------------")
+
+var removeElement = function(nums, val) {
+    let count = 0
+    for(let i = 0; i < nums.length; i++){
+      console.log("i is "+nums[i]+" "+nums)
+        if(nums[i] == val){
+            nums.splice(i, 1)
+            i--
+        } else {
+          count++;
+        }
+        
+    }
+
+    return nums
+};
+
+console.log(removeElement([3,2,2,3], 3)) //Output: 2, nums = [2,2]
+console.log(removeElement([0,1,2,2,3,0,4,2], 2)) //Output: 5, nums = [0,1,3,0,4]
+
 
